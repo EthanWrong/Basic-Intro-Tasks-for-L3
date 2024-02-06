5b65a13c2cbce7e3b9b7838717856b1c9e8b3d3b
EthanWrong <98923515+EthanWrong@users.noreply.github.com>
2024-02-07 10:06:18 +1300

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,4 +1,20 @@
 """"Problem 1 - Secret PIN"""
 
-pin = input("Enter your secret 4-digit PIN: ")  # will return a string
-print(f"Your secret PIN is {pin}")
+
+def check_pin(pin):
+    if len(pin) != 4:
+        return False
+    for i in pin:
+        try:
+            int(i)
+        except ValueError:
+            return False
+    return True
+
+
+my_pin = input("Enter your secret 4-digit PIN: ")  # will return a string
+
+if check_pin(my_pin):
+    print(f"Your secret PIN is {my_pin}")
+else:
+    print("Invalid PIN")
