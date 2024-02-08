a1c201a2f4cb94b705969e57a1cd8d63d9ed4929
EthanWrong <98923515+EthanWrong@users.noreply.github.com>
2024-02-09 11:36:07 +1300

    Update sandwich_maker.py

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -1,60 +1,5 @@
 """Revision Task - Sandwich Maker
-by Ethan Wong
-
-No error checking"""
-
-
-def print_options(menu):
-    for option in menu:
-        print(f"{option} {'':.<5} ${menu[option]:.2f}")
-    print(f"None {'':.<5} $0.00")
-
-
-def print_selection(menu, bread, meats, garnishes):
-    print("Bread")
-    print(f" {bread} {'':.<5} ${menu["bread"][bread]:.2f}")
-
-    print("Meats")
-    for meat in meats:
-        print(f" {meat} {'':.<5} ${menu["meat"][meat]:.2f}")
-
-    print("Garnishes")
-    for garnish in garnishes:
-        print(f" {garnish} {'':.<5} ${menu["garnish"][garnish]:.2f}")
-
-
-def select_bread(bread_menu):
-    print_options(bread_menu)
-    while True:
-        user = input("Select a bread option: ").strip().title()
-        if user in bread_menu.keys():
-            print(f"You have selected {user} bread")
-            return bread_menu[user]
-        else:
-            print("Please enter a valid option")
-
-
-def select_ingredients(ingredient_menu, ingredient):
-    menu = ingredient_menu
-    print_options(menu)
-    print(f"Skip ..... (no {ingredient})")
-    print(f"Done")
-
-    selection = []
-
-    while True:
-        user = input(f"Select a(nother) {ingredient} option: ").strip().title()
-        if user in menu.keys():
-            print(f"You have selected {user} {ingredient}")
-            selection.append(ingredient_menu[user])
-            return selection
-        elif user == "Done":
-            print(f"")
-        elif user in ("Skip", "X", "Cancel", "N", "No"):
-            return []
-        else:
-            print("Please enter a valid option")
-
+by Ethan Wong"""
 
 MY_MENU = {
     "bread": {
@@ -77,13 +22,90 @@ MY_MENU = {
     }
 }
 
-bread = select_bread(MY_MENU["bread"])
-print()
-ingredients = select_ingredients(MY_MENU["meat"], "meat")
-print()
-garnishes = select_ingredients(MY_MENU["garnish"], "garnish")
-print()
-print_selection(MY_MENU, bread, ingredients, garnishes)
+
+def print_options(options):
+    for option in options:
+        print(f" {option} {'':.<5} ${options[option]:.2f}")
+    print("Enter 'X' to move on")
 
 
+def select_ingredient(menu, ingredient, limit=100):
+    selection = []
+    print(f"{ingredient.title()} - Limit: {limit}")
+    print_options(menu[ingredient])
+    while True:
+        user = input(f"Select a {ingredient}: ").strip().title()
+        if user in menu[ingredient].keys():
+            selection.append([user, menu[ingredient][user]])
+        elif user == "X":
+            return selection
+        else:
+            print("Please enter a valid option")
 
+        if len(selection) >= limit:
+            return selection
+
+
+def print_order(order):
+    cost = 0.0
+    print(f"Your Order:")
+    for item in order:
+        cost += item[1]
+        print(f" {item[0]} {'':.<5} ${item[1]:.2f}")
+    print(f" Total Cost: ${cost:.2f}")
+    print()
+
+
+def edit_order(order, menu):
+    while True:
+        print("Edit Order:\n"
+              " Enter the name of the item you would like to remove\n"
+              " Enter 'bread' if you would like to change the bread type\n"
+              " Enter 'meat' or 'garnish' if you would like to add more\n"
+              " Enter 'Done' if you are happy with your order")
+        user = input(" > ").lower()
+        print()
+
+        item_names = []
+        for item in order:
+            item_names.append(item[0].lower())
+
+        if user in item_names:
+            order.pop(item_names.index(user))
+            print(f"You have removed {user.title()} from your order.")
+        elif user == "bread":
+            if order:
+                order[0] = select_ingredient(menu, "bread", 1)
+            else:
+                order += (select_ingredient(menu, "bread", 1))
+            print(f"You have selected {order[0][0]} bread.")
+        elif user in ("meat", "garnish"):
+            order += (select_ingredient(menu, user))
+            print(f"You have added {user.title()} to your order")
+        elif user in ("done", "complete", "finish", "end"):
+            print_order(order)
+            return order
+        else:
+            print("Please enter a valid option")
+
+        print()
+        print_order(order)
+
+# main routine
+
+
+print("Welcome to Sandwhich Maker, where all your sandwich dreams come true.")
+print()
+my_bread = select_ingredient(MY_MENU, "bread", 1)
+print()
+my_meat = select_ingredient(MY_MENU, "meat")
+print()
+my_garnish = select_ingredient(MY_MENU, "garnish")
+print()
+
+my_order = my_bread + my_meat + my_garnish
+print_order(my_order)
+
+my_order = edit_order(my_order, MY_MENU)
+
+print("Thank you for using Sandwich Maker")
